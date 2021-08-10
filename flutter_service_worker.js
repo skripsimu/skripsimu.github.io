8c1e523f99ba3fb612ae8139c68a1a6eb81e0a56
tegar <tegar.nugroho@danafix.id>
2021-08-10 17:35:02 +0700

    update dialog

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "77da638a4c2168d5f160b23474502f00",
-"index.html": "f35ff4599bf84a1f6479da9e0dc1d958",
-"/": "f35ff4599bf84a1f6479da9e0dc1d958",
-"main.dart.js": "39e41f5bb3ab40afcb76314ff3fdfcf5",
+"index.html": "062d03498e96e720233324d1e09ca080",
+"/": "062d03498e96e720233324d1e09ca080",
+"main.dart.js": "869e0c2e9d4956b76dfba5db51721694",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
