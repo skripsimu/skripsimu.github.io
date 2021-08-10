7e3f1e28607558bb83cfccc7e9e5e160c762c2e9
tegar <tegar.nugroho@danafix.id>
2021-08-10 11:18:08 +0700

    change to imahe network

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "77da638a4c2168d5f160b23474502f00",
-"index.html": "c0de0d70e64f2ec9609cfcaff381c046",
-"/": "c0de0d70e64f2ec9609cfcaff381c046",
-"main.dart.js": "2fe160aa751462c4e98f4f9f9bf2f2b1",
+"index.html": "40a0c57d30e8deec86c6b22b2b1a97d5",
+"/": "40a0c57d30e8deec86c6b22b2b1a97d5",
+"main.dart.js": "5ebfb5428d4581864708080c11425e29",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
