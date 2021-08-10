b56cd486b9ef5e723e2372db2060878cd4bbb0ff
tegar <tegar.nugroho@danafix.id>
2021-08-10 11:06:49 +0700

    try again

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "77da638a4c2168d5f160b23474502f00",
-"index.html": "dfa5b695bc7cb23a9a970fb3edaef8c8",
-"/": "dfa5b695bc7cb23a9a970fb3edaef8c8",
-"main.dart.js": "17702d3857b0fb6c0ee0c127128d26fd",
+"index.html": "c0de0d70e64f2ec9609cfcaff381c046",
+"/": "c0de0d70e64f2ec9609cfcaff381c046",
+"main.dart.js": "2fe160aa751462c4e98f4f9f9bf2f2b1",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
